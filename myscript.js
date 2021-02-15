e53ff9d5feaa52ddf18ff6025366cb6125637b6a
Riccardo <riccardo@baako.de>
2021-02-15 21:02:00 +0100

    declared consts

---
+++ myscript.js
@@ -0,0 +1,16 @@
+const triggers = document.querySelectorAll('.cool > li');
+const background = document.querySelector('.dropdownBackground');
+const nav = document.querySelector('top');
+
+function handleEnter(){
+
+
+}
+
+function handleLeave() {
+
+
+}
+
+triggers.forEach(triggers => trigger.addEventListener('mouseenter', handleEnter));
+triggers.forEach(triggers => trigger.addEventListener('mouseleave', handleLeave));
