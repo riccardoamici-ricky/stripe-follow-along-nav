d6f226df73bec3ca1b2a5f4ba4e18183c1a54ded
Riccardo <riccardo@baako.de>
2021-02-15 21:10:29 +0100

    function handleenter

--- myscript.js
+++ myscript.js
@@ -3,7 +3,8 @@ const background = document.querySelector('.dropdownBackground');
 const nav = document.querySelector('top');
 
 function handleEnter(){
-
+  this.classList.add('trigger-enter');
+  setTimeout(() => this.classList.add('trigger-enter-active'), 150)
 
 }
 
